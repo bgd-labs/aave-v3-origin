95f6c9499c76fff81eb98db0c9f68956feb08779
sakulstra <lukasstrassel@googlemail.com>
2024-09-09 09:33:54 +0200

    fix: cerota ci

--- tests/periphery/static-a-token/ERC4626StataTokenUpgradeable.t.sol
+++ tests/periphery/static-a-token/ERC4626StataTokenUpgradeable.t.sol
@@ -60,10 +60,6 @@ contract ERC4626StataTokenUpgradeableTest is TestnetProcedures {
     assertEq(erc4626Upgradeable.previewRedeem(shares), assets);
   }
 
-  function test_totalAssets_shouldbeZeroOnZeroSupply() external {
-    assertEq(erc4626Upgradeable.totalAssets(), 0);
-  }
-
   // ### DEPOSIT TESTS ###
   function test_depositATokens(uint128 assets, address receiver) public {
     _validateReceiver(receiver);
